59a53cff0090f966cdf6578be712e7a23e0af66e
yishin0819 <yishin0819@gmail.com>
2016-02-29 14:10:31 +0000

    add upvote

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -60,12 +60,18 @@ class TopicsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+   def upvote
+      @topic = Topic.find(params[:id])
+      @topic.votes.create
+      redirect_to(topics_path)
+    end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_topic
       @topic = Topic.find(params[:id])
     end
+
+ 
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def topic_params
